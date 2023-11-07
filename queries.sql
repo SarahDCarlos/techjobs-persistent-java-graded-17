45dbd228768acd453356b2128aa40ae46af8327e
Sarah Carlos <SarahDCarlos@outlook.com>
2023-11-06 23:16:50 -0600

    Updated Models, Data Layer, and Controllers

--- queries.sql
+++ queries.sql
@@ -1,6 +1,11 @@
 --Part 1
-
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'job';
 --Part 2
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 --Part 3
 
